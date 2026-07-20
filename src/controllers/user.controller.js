12c8f522bae390d572e4ecabd250d0a852e3cfe6
Aniket Gupta <omgrealme962@gmail.com>
2026-07-20 14:39:16 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import { $size } from "sift";
 
 
 const generateAccessAndRefereshTokens = async(userId) =>  {
@@ -251,7 +252,7 @@ const logoutUser = asyncHandler(async(req, res) => {
       throw new ApiError(400, "All fields are required")
      }
 
-     const user = User.findByIdAndUpdate(
+     const user = await User.findByIdAndUpdate(
       req.user?._id,
       {
         $set: {
@@ -274,6 +275,9 @@ const logoutUser = asyncHandler(async(req, res) => {
     if(!avatarLocalPath) {
       throw new ApiError(400, "Avatar file is missing")
     }
+
+    //TODO: delete old image - assignment
+    
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
 
@@ -333,7 +337,78 @@ const logoutUser = asyncHandler(async(req, res) => {
 
    })
     
+     const getUserChannelProfile = asyncHandler(async(req, res) => {
+      const {username} = req.params
 
+      if (!username?.trim()) {
+        throw new ApiError(400, "username is missing")
+        }
+
+         const channel = await User.aggregate([
+          {
+            $match: {
+              username: username?.toLowerCase()
+            }
+          },
+          {
+            
+              $lookup: {
+                from: "subscription",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+              }
+          },
+          {
+            
+              $lookup: {
+                from: "subscription",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+              }
+          },
+          {
+            $addFields: {
+              subscribersCount: {
+                $size: "$subscribers"
+              },
+              channelsSubscribeToCount: {
+                $size: "$subscribedTo"
+              },
+              isSubscribed: {
+                $cond: {
+                  if: {$in: [req.user?._id, "subscribers.subscriber"]},
+                  then: true,
+                  else: false
+                }
+              }
+            }
+          },
+
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              subscribersCount: 1,
+              channelsSubscribeToCount: 1,
+              isSubscribed: 1,
+              avatar: 1,
+              coverImage: 1,
+              email: 1
+
+            }
+          }
+         ])
+
+         if (!channel?.length) {
+            throw new ApiError(400, "channel does not exists")
+         }
+
+         return res.status(200)
+         .json(new ApiResponse(200, channel[0], "User channnel fetched successfully"))
+
+      })
 
 
 export {
