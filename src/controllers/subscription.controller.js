48b965a2fc303baceab798770f28b7591800820a
Aniket Gupta <omgrealme962@gmail.com>
2026-07-25 01:23:12 +0530

    Subscription.controller.js complete

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -46,13 +46,55 @@ return res.status(200).json(
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-})
+    const { channelId } = req.params;
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel id");
+    }
+
+    const subscribers = await Subscription.find({
+        channel: channelId
+    }).populate(
+        "subscriber",
+        "username fullName avatar"
+    );
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            subscribers,
+            "Subscribers fetched successfully"
+        )
+    );
+});
+
+
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+    const { subscriberId } = req.params;
+
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriber id");
+    }
+
+    const subscribedChannels = await Subscription.find({
+        subscriber: subscriberId
+    }).populate(
+        "channel",
+        "username fullName avatar"
+    );
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            subscribedChannels,
+            "Subscribed channels fetched successfully"
+        )
+    );
+});
+
+
 
 export {
     toggleSubscription,
